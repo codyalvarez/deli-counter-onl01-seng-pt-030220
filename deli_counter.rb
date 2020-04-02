d56ca7ffc6a3c00b4435c23686e8d76272b53e45
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-04-02 17:17:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(customer_array)
   if customer_array.count == 0
     puts "The line is currently empty."
   else
-    phrase = "The line is currently empty."
+    phrase = "The line is currently:"
     customer_array.each_with_index { |customer, index| phrase +="#{index+1}. #{customer}"}
     puts phrase
   end
